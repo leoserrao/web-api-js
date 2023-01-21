ce1a0dfb484f4524b703d25aa8c67644f82fc528
Léo Serrão <leo_serrao@hotmail.com>
2023-01-21 12:03:35 -0300

    criando o handler

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 const http = require('http')
 const PORT = 3000
 const handler = (request, response) => {
-
+    const { url, method } = request
+    console.log({
+        url, method
+    })
+    response.end()
 }
 
 http.createServer(handler)
