df47e3c5b70d3b205f89602dcde8e2609b1df4b4
Léo Serrão <leo_serrao@hotmail.com>
2023-01-21 09:54:59 -0300

    iniciando a criação da API

---
+++ src/index.js
@@ -0,0 +1,8 @@
+const http = require('http')
+const PORT = 3000
+const handler = (request, response) => {
+
+}
+
+http.createServer(handler)
+    .listen(PORT, () => console.log('Server running at', PORT))
